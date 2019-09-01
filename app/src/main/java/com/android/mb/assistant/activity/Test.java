aacb344bbc194dc84daca7d11769dbc4fc54d0cd
chenqm <529387306@qq.com>
2019-09-01 17:42:56 +0800

    Revert "5"
    This reverts commit ce9c61c3

--- app/src/main/java/com/android/mb/assistant/activity/Test.java
+++
@@ -1,7 +0,0 @@
-package com.android.mb.assistant.activity;
-
-/**
- * v4
- */
-public class Test {
-}
--- app/src/main/java/com/android/mb/assistant/activity/Test1.java
+++
@@ -1,7 +0,0 @@
-package com.android.mb.assistant.activity;
-
-/**
- * v8
- */
-public class Test1 {
-}
